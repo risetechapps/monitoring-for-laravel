f7b81c9ee786138102c8fb9b0037b5f3e94bc503
risetechapps <apps@risetech.com.br>
2025-04-19 00:20:16 -0300

    Registrado macro

--- src/MonitoringServiceProvider.php
+++ src/MonitoringServiceProvider.php
@@ -3,6 +3,7 @@
 namespace RiseTechApps\Monitoring;
 
 use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use RiseTechApps\Monitoring\Features\Device\Device;
@@ -63,5 +64,44 @@ class MonitoringServiceProvider extends ServiceProvider
         $this->app->singleton(Device::class, function ($app) {
             return new Device();
         });
+
+        $this->registerMacros();
+    }
+
+    protected function registerMacros(): void
+    {
+
+        if(!ResponseFactory::hasMacro('jsonSuccess')){
+            ResponseFactory::macro('jsonSuccess', function ($data = []) {
+                $response = ['success' => true];
+                if (!empty($data)) $response['data'] = $data;
+                return response()->json($response);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonError')){
+            ResponseFactory::macro('jsonError', function ($data = null) {
+                $response = ['success' => false];
+                if (!is_null($data)) $response['message'] = $data;
+                return response()->json($response, 412);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonGone')) {
+            ResponseFactory::macro('jsonGone', function ($data = null) {
+                $response = ['success' => false];
+                if (!is_null($data)) $response['message'] = $data;
+                return response()->json($response, 410);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonNotValidated')) {
+            ResponseFactory::macro('jsonNotValidated', function ($message = null, $error = null) {
+                $response = ['success' => false];
+                if (!is_null($message)) $response['message'] = $message;
+
+                return response()->json($response, 422);
+            });
+        }
     }
 }
